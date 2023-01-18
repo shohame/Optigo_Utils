4d46304e60d756dec8d4093099472082cc0efb0a
Perry Shoham <Shoham Perry ELT 74618 a85571@elta.iai>
2023-01-18 13:49:24 +0200

    Cosmetic changes in build_package.py.

--- build_package.py
+++ build_package.py
@@ -3,16 +3,24 @@ import os
 from pathlib import Path
 import shutil
 
-cmd_str = 'python setup.py build --build-base ../outputs egg_info --egg-base ../outputs bdist_wheel --universal --dist-dir ../outputs'
-#os.chdir('OptigoUtils')
+out_path_str = '../outputs'
+optigo_pip_path = 'N:/Optigoprj/pip'
+package_name = 'OptigoUtils'
+
+out_path = Path(out_path_str)
+if out_path.exists():
+    shutil.rmtree(out_path)
+
+cmd_str = f'python setup.py build --build-base {out_path_str} ' + \
+          f'egg_info --egg-base {out_path_str} ' + \
+          f'bdist_wheel --universal --dist-dir {out_path_str}'
 os.system(cmd_str)
 
+src_path = out_path
 
-src_path = Path(r'../outputs')
+src_file_path = list(src_path.glob(f'{package_name}*.whl'))[0]
 
-src_file_path = list(src_path.glob('OptigoUtils*.whl'))[0]
-
-dest_path = Path(r'N:/Optigoprj/pip')
+dest_path = Path(optigo_pip_path)
 
 dest_file_path = dest_path / src_file_path.name
 
